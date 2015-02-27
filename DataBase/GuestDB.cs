4ec53f5408e6c9160c75c66ebf395decc6e31e0d
cubano1985 <pawel.solowczuk@gmail.com>
2015-02-27 13:05:20 +0100

    created Model and DB

---
+++ DataBase/GuestDB.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models;
+
+namespace DataBase
+{
+    public class GuestDB : IGuestDb
+    {
+        public List<Guest> GuestList { get; set; }
+    }
+}
---
+++ Models/Guest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Guest
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public GuestGender Gender { get; set; }
+        public GuestStatus Status { get; set; }
+
+    }
+}
